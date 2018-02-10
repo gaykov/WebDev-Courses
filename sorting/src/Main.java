64c08a23035a8ab0de49a1c2571bb9ee5c5830fa
AlexandraKoz <alejandrakoz@mail.ru>
2018-02-10 16:38:54 +0300

    Add my array sort

---
+++ sorting/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int []array = {52, 13, 88, 100, 158, 96, 21, 33, 87};
+       Sort sort = new Sort();
+       sort.sortArray(array);
+    }
+}
